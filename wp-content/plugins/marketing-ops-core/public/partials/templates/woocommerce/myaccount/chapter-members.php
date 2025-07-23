f39522a413209101a91f0e0f8d3abb0837808d99
Adarsh Verma <adarsh@concatstring.com>
2025-07-23 21:09:21 +0530

    working on chapters

--- wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/chapter-members.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/woocommerce/myaccount/chapter-members.php
@@ -9,13 +9,12 @@
 defined( 'ABSPATH' ) || exit;
 
 $my_orders_columns = apply_filters(
-	'woocommerce_my_account_my_orders_columns',
+	'woocommerce_my_account_chapter_members_columns',
 	array(
-		'order-number'  => esc_html__( 'Order', 'woocommerce' ),
-		'order-date'    => esc_html__( 'Date', 'woocommerce' ),
-		'order-status'  => esc_html__( 'Status', 'woocommerce' ),
-		'order-total'   => esc_html__( 'Total', 'woocommerce' ),
-		'order-actions' => '&nbsp;',
+		'picture' => '&nbsp;',
+		'name'    => __( 'Date', 'woocommerce' ),
+		'email'   => __( 'Status', 'woocommerce' ),
+		'actions' => '&nbsp;',
 	)
 );
 
@@ -33,11 +32,8 @@ $customer_orders = get_posts(
 );
 
 if ( $customer_orders ) : ?>
-
-	<h2><?php echo apply_filters( 'woocommerce_my_account_my_orders_title', esc_html__( 'Recent orders', 'woocommerce' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></h2>
-
+	<h2><?php echo apply_filters( 'woocommerce_my_account_chapter_members_title', esc_html__( 'Chapter members', 'woocommerce' ) ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></h2>
 	<table class="shop_table shop_table_responsive my_account_orders">
-
 		<thead>
 			<tr>
 				<?php foreach ( $my_orders_columns as $column_id => $column_name ) : ?>
@@ -58,33 +54,19 @@ if ( $customer_orders ) : ?>
 							<?php if ( has_action( 'woocommerce_my_account_my_orders_column_' . $column_id ) ) : ?>
 								<?php do_action( 'woocommerce_my_account_my_orders_column_' . $column_id, $order ); ?>
 
-							<?php elseif ( 'order-number' === $column_id ) : ?>
+							<?php elseif ( 'picture' === $column_id ) : ?>
 								<a href="<?php echo esc_url( $order->get_view_order_url() ); ?>">
 									<?php echo _x( '#', 'hash before order number', 'woocommerce' ) . $order->get_order_number(); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>
 								</a>
 
-							<?php elseif ( 'order-date' === $column_id ) : ?>
+							<?php elseif ( 'name' === $column_id ) : ?>
 								<time datetime="<?php echo esc_attr( $order->get_date_created()->date( 'c' ) ); ?>"><?php echo esc_html( wc_format_datetime( $order->get_date_created() ) ); ?></time>
 
-							<?php elseif ( 'order-status' === $column_id ) : ?>
+							<?php elseif ( 'email' === $column_id ) : ?>
 								<?php echo esc_html( wc_get_order_status_name( $order->get_status() ) ); ?>
 
-							<?php elseif ( 'order-total' === $column_id ) : ?>
-								<?php
-								/* translators: 1: formatted order total 2: total order items */
-								printf( _n( '%1$s for %2$s item', '%1$s for %2$s items', $item_count, 'woocommerce' ), $order->get_formatted_order_total(), $item_count ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-								?>
-
-							<?php elseif ( 'order-actions' === $column_id ) : ?>
-								<?php
-								$actions = wc_get_account_orders_actions( $order );
-
-								if ( ! empty( $actions ) ) {
-									foreach ( $actions as $key => $action ) { // phpcs:ignore WordPress.WP.GlobalVariablesOverride.Prohibited
-										echo '<a href="' . esc_url( $action['url'] ) . '" class="button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
-									}
-								}
-								?>
+							<?php elseif ( 'actions' === $column_id ) : ?>
+								<a href="/" class="button <?php echo sanitize_html_class( $key ); ?>"><?php esc_html_e( 'Check profile', 'marketingops' ); ?></a>
 							<?php endif; ?>
 						</td>
 					<?php endforeach; ?>
