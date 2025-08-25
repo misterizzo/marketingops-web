79dffd794eaccea31a62ed215ddaa515be668e78
meggira <kumbharanameghna@gmail.com>
2025-08-25 23:42:41 +0530

    add label after input

--- wp-content/plugins/marketing-ops-core/public/partials/templates/users/profile-setup.php
+++ wp-content/plugins/marketing-ops-core/public/partials/templates/users/profile-setup.php
@@ -113,7 +113,8 @@ if ( ! empty( $member_plan_obj[0]->plan_id ) ) {
 							<div class="form_row">
 								<!-- input with error -->
 								<div class="content_boxed moc_required_field">
-									<input type="text" class="inputtext" name="moc_first_name" placeholder="First Name" value="<?php echo esc_html( $first_name ); ?>">
+									<input type="text" class="inputtext" name="moc_first_name" placeholder="" value="<?php echo esc_html( $first_name ); ?>">
+									<span class="label">First Name</span>
 									<div class="moc_error moc_first_name_err">
 										<span></span>
 									</div>
@@ -124,7 +125,8 @@ if ( ! empty( $member_plan_obj[0]->plan_id ) ) {
 							<div class="form_row">
 								<!-- input with error -->
 								<div class="content_boxed moc_required_field">
-									<input type="text" class="inputtext" name="moc_last_name" placeholder="Last name" value="<?php echo esc_html( $last_name ); ?>">
+									<input type="text" class="inputtext" name="moc_last_name" placeholder="" value="<?php echo esc_html( $last_name ); ?>">
+									<span class="label">Last name</span>
 									<div class="moc_error moc_last_name_err">
 										<span></span>
 									</div>
@@ -135,7 +137,8 @@ if ( ! empty( $member_plan_obj[0]->plan_id ) ) {
 							<div class="form_row">
 								<!-- input with error -->
 								<div class="content_boxed">
-									<input type="text" class="inputtext" name="moc_name_pronunciation" placeholder="He/Him/She/Her" value="<?php echo esc_html( $pronunciation ); ?>">
+									<input type="text" class="inputtext" name="moc_name_pronunciation" placeholder="" value="<?php echo esc_html( $pronunciation ); ?>">
+									<span class="label">He/Him/She/Her</span>
 								</div>
 							</div>
 						</div>
@@ -211,7 +214,8 @@ if ( ! empty( $member_plan_obj[0]->plan_id ) ) {
 					<div class="form_row">
 						<!-- input with error -->
 						<div class="content_boxed">
-							<input type="text" class="inputtext" name="moc_pro_text" placeholder="Professional title"  value="<?php echo esc_html( $profetional_title ); ?>">
+							<input type="text" class="inputtext" name="moc_pro_text" placeholder=""  value="<?php echo esc_html( $profetional_title ); ?>">
+							<span class="label">Professional title</span>
 						</div>
 					</div>
 
